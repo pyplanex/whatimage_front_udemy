dcc9aec9c5321f74a20ffc2e6d9ecd95391dee57
Łukasz Mąkinia <lukaszmakinia@iMac-ukasz.local>
2020-01-13 16:01:24 +0100

    ready front project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Classifier from './components/Classifier/Classifier';
+import ImageLIst from './components/ImageList/ImageList';
+import Navigation from './components/Navigation/Navigation';
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Classifier} />
+          <Route exact path='/list' component={ImageLIst} />
+          <Route exact path='*' component={Classifier} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
